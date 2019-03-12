72a60f5cdd4799e3345cb1129eace659976018ce
Ivan Belokobylskiy <belokobylskij@gmail.com>
2019-03-12 12:06:58 +0300

    Extract helpers to functions

--- sentry.py
+++ sentry.py
@@ -7,54 +7,56 @@ import io
 import json
 
 
+def get_exception_str(exception):
+    exception_io = io.StringIO()
+    sys.print_exception(exception, exception_io)
+    exception_io.seek(0)
+    result = exception_io.read()
+    exception_io.close()
+    return result
+
+
+async def http_request(domain, url, data, headers=()):
+    reader, writer = await asyncio.open_connection(domain, 80)
+    method = 'GET'
+    if data:
+        method = 'POST'
+        body_extra =  \
+            "Content-Length: {}\r\n\r\n{}".format(
+                len(data.encode('latin-1')),
+                data,
+            )
+    else:
+        body_extra = '\r\n'
+    headers_str = ''.join(
+        "{}: {}\r\n".format(k, v) for k, v in headers.items()
+    )
+
+    query = \
+        "{} {} HTTP/1.0\r\n" \
+        "Host: {}\r\n" \
+        "{}" \
+        "{}".format(
+            method,
+            url,
+            domain,
+            headers_str,
+            body_extra,
+        )
+    try:
+        await writer.awrite(query.encode('latin-1'))
+        while True:
+            line = await reader.readline()
+            if not line:
+                break
+    except:
+        pass
+
+
 class SentryClient:
     def __init__(self, project_id, key):
         self.project_id = project_id
         self.key = key
-
-    async def http_request(self, domain, url, data, headers=()):
-        reader, writer = await asyncio.open_connection(domain, 80)
-        method = 'GET'
-        if data:
-            method = 'POST'
-            body_extra =  \
-                "Content-Length: {}\r\n\r\n{}".format(
-                    len(data.encode('latin-1')),
-                    data,
-                )
-        else:
-            body_extra = '\r\n'
-        headers_str = ''.join(
-            "{}: {}\r\n".format(k, v) for k, v in headers.items()
-        )
-
-        query = \
-            "{} {} HTTP/1.0\r\n" \
-            "Host: {}\r\n" \
-            "{}" \
-            "{}".format(
-                method,
-                url,
-                domain,
-                headers_str,
-                body_extra,
-            )
-        try:
-            await writer.awrite(query.encode('latin-1'))
-            while True:
-                line = await reader.readline()
-                if not line:
-                    break
-        except:
-            pass
-
-    def get_exception_str(self, exception):
-        exception_io = io.StringIO()
-        sys.print_exception(exception, exception_io)
-        exception_io.seek(0)
-        result = exception_io.read()
-        exception_io.close()
-        return result
 
     async def send_exception(self, exception):
         domain = 'sentry.io'
@@ -79,7 +81,7 @@ class SentryClient:
                     exception.args[0] if exception.args else '',
                 ),
                 'module': exception,
-                'stacktrace': json.dumps(self.get_exception_str(exception)),
+                'stacktrace': json.dumps(get_exception_str(exception)),
                 'machine_id': binascii.hexlify(machine.unique_id()).decode(),
                 'platform': sys.platform,
                 'os_name': sys.implementation.name,
@@ -88,7 +90,7 @@ class SentryClient:
                 ),
             }
         )
-        return await self.http_request(
+        return await http_request(
             domain,
             url,
             json_data,
